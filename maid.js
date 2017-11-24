247a9d6eb2bfbd697354a854ef06aa1b33f16d22
ThePwnzr <jordanrmasek@gmail.com>
2017-11-24 14:49:00 -0500

    added and removed a lot of semi-colons

--- maid.js
+++ maid.js
@@ -1,3 +1,4 @@
+/*jshint esversion:6*/
 //The bot kinda needs these things to do things.
 const Discord = require("discord.js");
 const fs = require("fs");
@@ -10,7 +11,7 @@ sql.open("./score.sqlite");
 
 //Ready to go!
 client.on("ready", () => {
-  client.user.setGame("==help for more info")
+  client.user.setGame("==help for more info");
   console.log("Chu ~!");
 });
 
@@ -43,7 +44,7 @@ function cycles() {
     }
     else if (cycleHours == 0 && cycleDays == 0){
       if (cycleMinutes == 1) {
-        cycleTotal = "I've been awake for " + cycleMinutes + " minute ~! Just starting my day."
+        cycleTotal = "I've been awake for " + cycleMinutes + " minute ~! Just starting my day.";
       } else {
         cycleTotal = "I've been awake for " + cycleMinutes + " minutes ~! Just starting my day.";
     }}
@@ -53,19 +54,19 @@ function cycles() {
       //Precision.
       if (cycleHours == 1) {
         if (cycleMinutes == 0) {
-          cycleTotal = "I've been awake for " + (cycleHours) + " hour ~! Ready to serve!"
+          cycleTotal = "I've been awake for " + (cycleHours) + " hour ~! Ready to serve!";
         } else if (cycleMinutes == 1) {
-          cycleTotal = "I've been awake for " + (cycleHours) + " hour and " + (cycleMinutes) + " minute ~! Ready to serve!"
+          cycleTotal = "I've been awake for " + (cycleHours) + " hour and " + (cycleMinutes) + " minute ~! Ready to serve!";
         } else if (cycleMinutes > 1) {
-          cycleTotal = "I've been awake for " + (cycleHours) + " hour and " + (cycleMinutes) + " minutes ~! Ready to serve!"
+          cycleTotal = "I've been awake for " + (cycleHours) + " hour and " + (cycleMinutes) + " minutes ~! Ready to serve!";
         }
       } else if (cycleHours > 1) {
         if (cycleMinutes == 0) {
-          cycleTotal = "I've been awake for " + (cycleHours) + " hours ~! Ready to serve!"
+          cycleTotal = "I've been awake for " + (cycleHours) + " hours ~! Ready to serve!";
         } else if (cycleMinutes == 1) {
-          cycleTotal = "I've been awake for " + (cycleHours) + " hours and " + (cycleMinutes) + " minute ~! Ready to serve!"
+          cycleTotal = "I've been awake for " + (cycleHours) + " hours and " + (cycleMinutes) + " minute ~! Ready to serve!";
         } else if (cycleMinutes > 1) {
-          cycleTotal = "I've been awake for " + (cycleHours) + " hours and " + (cycleMinutes) + " minutes ~! Ready to serve!"
+          cycleTotal = "I've been awake for " + (cycleHours) + " hours and " + (cycleMinutes) + " minutes ~! Ready to serve!";
         }
       }
     }
@@ -82,9 +83,9 @@ function cycles() {
         if (cycleDays == 1 && cycleMinutes == 0) {
         cycleTotal = "I've been awake for " + cycleDays + " day and " + cycleMinutes + " minute ~! Ready to serve!";
       } else if (cycleDays == 1 && cycleMinutes > 1) {
-        cycleTotal = "I've been awake for " + cycleDays + " day and " + cycleMinutes + " minutes ~! Ready to serve!"
+        cycleTotal = "I've been awake for " + cycleDays + " day and " + cycleMinutes + " minutes ~! Ready to serve!";
       } else if (cycleDays > 1 && cycleMinutes > 1){
-        cycleTotal = "I've been awake for " + cycleDays + " days and " + cycleMinutes + " minutes ~! Ready to serve!"
+        cycleTotal = "I've been awake for " + cycleDays + " days and " + cycleMinutes + " minutes ~! Ready to serve!";
       }}
       else if (cycleHours > 0 && cycleMinutes == 0) {
         if (cycleDays == 1 && cycleHours == 1) {
@@ -97,26 +98,26 @@ function cycles() {
       else if (cycleHours > 0 && cycleMinutes > 0) {
         if (cycleDays == 1) {
           if (cyclehours == 1 && cycleMinutes > 1) {
-            cycleTotal = "I've been awake for " + cycleDays + " day, " + cycleHours + " hour, and " + (cycleMinutes) + " minutes ~! Ready to serve! But I sure am sleepy...."
+            cycleTotal = "I've been awake for " + cycleDays + " day, " + cycleHours + " hour, and " + (cycleMinutes) + " minutes ~! Ready to serve! But I sure am sleepy....";
           } else if (cyclehours > 1 && cycleMinutes == 1) {
-            cycleTotal = "I've been awake for " + cycleDays + " day, " + cycleHours + " hours, and " + (cycleMinutes) + " minute ~! Ready to serve! But I sure am sleepy...."
+            cycleTotal = "I've been awake for " + cycleDays + " day, " + cycleHours + " hours, and " + (cycleMinutes) + " minute ~! Ready to serve! But I sure am sleepy....";
           } else if (cycleHours == 1 && cycleMinutes == 1) {
-            cycleTotal = "I've been awake for " + cycleDays + " day, " + cycleHours + " hour, and " + (cycleMinutes) + " minute ~! Ready to serve! But I sure am sleepy...."
+            cycleTotal = "I've been awake for " + cycleDays + " day, " + cycleHours + " hour, and " + (cycleMinutes) + " minute ~! Ready to serve! But I sure am sleepy....";
           } else if (cycleHours > 1 && cycleMinutes > 1) {
-            cycleTotal = "I've been awake for " + cycleDays + " day, " + cycleHours + " hours, and " + (cycleMinutes) + " minutes ~! Ready to serve! But I sure am sleepy...."
+            cycleTotal = "I've been awake for " + cycleDays + " day, " + cycleHours + " hours, and " + (cycleMinutes) + " minutes ~! Ready to serve! But I sure am sleepy....";
           }
         } else if (cycleDays > 1) {
           if (cyclehours == 1 && cycleMinutes > 1) {
-            cycleTotal = "I've been awake for " + cycleDays + " days, " + cycleHours + " hour, and " + (cycleMinutes) + " minutes ~! Ready to serve! But I sure am sleepy...."
+            cycleTotal = "I've been awake for " + cycleDays + " days, " + cycleHours + " hour, and " + (cycleMinutes) + " minutes ~! Ready to serve! But I sure am sleepy....";
           } else if (cyclehours > 1 && cycleMinutes == 1) {
-            cycleTotal = "I've been awake for " + cycleDays + " days, " + cycleHours + " hours, and " + (cycleMinutes) + " minute ~! Ready to serve! But I sure am sleepy...."
+            cycleTotal = "I've been awake for " + cycleDays + " days, " + cycleHours + " hours, and " + (cycleMinutes) + " minute ~! Ready to serve! But I sure am sleepy....";
           } else if (cycleHours == 1 && cycleMinutes == 1) {
-            cycleTotal = "I've been awake for " + cycleDays + " days, " + cycleHours + " hour, and " + (cycleMinutes) + " minute ~! Ready to serve! But I sure am sleepy...."
+            cycleTotal = "I've been awake for " + cycleDays + " days, " + cycleHours + " hour, and " + (cycleMinutes) + " minute ~! Ready to serve! But I sure am sleepy....";
           } else if (cycleHours > 1 && cycleMinutes > 1) {
-            cycleTotal = "I've been awake for " + cycleDays + " days, " + cycleHours + " hours, and " + (cycleMinutes) + " minutes ~! Ready to serve! But I sure am sleepy...."
+            cycleTotal = "I've been awake for " + cycleDays + " days, " + cycleHours + " hours, and " + (cycleMinutes) + " minutes ~! Ready to serve! But I sure am sleepy....";
           }
         }
-      };
+      }
     }
 
     //Wait the proper amount of time before updating. This needs to be at the end, as anything below likely won't get done.
@@ -131,21 +132,21 @@ client.on("message", function(message) {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   //This variable is mainly used for housekeeping, but I don't have it applied everywhere.
-  var player = message.author
+  var player = message.author;
 
   //commands are in if statements
   //Throw-away command. It's just here to make sure that everything works right.
   if (command === "ping") {
     message.channel.send("Pong ~! :ping_pong:");
-    message.channel.send(cycleTotal)
-  };
+    message.channel.send(cycleTotal);
+  }
   if (command === "wl") { if (!args[0]) {
     //I know what this command does, but I have no idea how to use sqlite as a whole.
     //Since sqlite is basically just better arrays (and is also promise-based) this command is needed to fetch the updated information.
     sql.get(`SELECT * FROM score WHERE userId ="${message.author.id}"`).then(row => {
       if (!row) {
         //The fact that there's even an "if" here is unnecessary. Don't feel like cleaning it up just yet.
-        return message.reply("You haven't won any games yet....")
+        return message.reply("You haven't won any games yet....");
       } else {
         message.channel.send({embed: {
           author: {
@@ -170,16 +171,16 @@ client.on("message", function(message) {
             name: "Win Percentage",
             value: row.percent + "%"
           }]
-        }})
+        }});
       }
     }).catch(() => {
       //If it catches something wrong, it'll make a sqlite array for the person that doesn't have one.
       console.error;
       sql.run("CREATE TABLE IF NOT EXISTS score (userId TEXT, wins INTEGER, losses INTEGER, percent INTEGER, matches INTEGER)").then(() => {
-        sql.run("INSERT INTO score (userId, wins, losses, percent, matches) VALUES (?, ?, ?, ?, ?)", [player.id, 0, 0, 0, 0])
+        sql.run("INSERT INTO score (userId, wins, losses, percent, matches) VALUES (?, ?, ?, ?, ?)", [player.id, 0, 0, 0, 0]);
       });
-      message.reply("You now have a stats page ~! Type '==wl' again to see it ~<3")
-    }); return
+      message.reply("You now have a stats page ~! Type '==wl' again to see it ~<3");
+    }); return;
   } else {
     //This is just the other form of ==wl.
     let member = message.mentions.members.first();
@@ -205,13 +206,13 @@ client.on("message", function(message) {
           name: "Win Percentage",
           value: row.percent + "%"
         }]
-      }})
+      }});
     }).catch(() => {
       //It won't make a registration for someone because they might not want to be registered.
       console.error;
-      message.reply(`I can't track someone who's not registered....`)
-      })}
-  };
+      message.reply(`I can't track someone who's not registered....`);
+    });}
+  }
 
   if (command === "awl") {
     //This thing is a mess.
@@ -221,38 +222,38 @@ client.on("message", function(message) {
       if (wl === "w") {
         //These console logs won't be necessary when everything is done.
         //Mainly to keep track of if the calculations are firing right.
-        console.log(row.wins + (args[2] * 1))
+        console.log(row.wins + (args[2] * 1));
         //I've noticed that when I turned these into variables, they stacked on each other.
-        console.log((row.wins + (args[2] * 1)) + (row.losses * 1))
+        console.log((row.wins + (args[2] * 1)) + (row.losses * 1));
         //When I made "args[2] * 1" a var, it added itself onto the number, not into.
-        console.log((row.wins + (args[2] * 1)) / ((row.wins + (args[2] * 1)) + (row.losses * 1)))
+        console.log((row.wins + (args[2] * 1)) / ((row.wins + (args[2] * 1)) + (row.losses * 1)));
         //If row.wins = 1 AND a = (arg[2] * 1) = 5, then row.wins + a = 15.
         sql.run(`UPDATE score SET wins = ${row.wins + (args[2] * 1)},
         matches = ${(row.wins + (args[2] * 1)) + (row.losses * 1)},
         percent = ${Math.floor((row.wins + (args[2] * 1)) / ((row.wins + (args[2] * 1)) + (row.losses * 1)) * 1000) / 10}
-        WHERE userId = ${member.id}`)
+        WHERE userId = ${member.id}`);
         //Which obviously isn't what I want.
         //Until we can figure out a way to make it not do that and make it look neater, this is what we're working with.
-      };
+      }
       if (wl === "l") {
         //See above.
-        console.log(row.losses + (args[2] * 1))
-        console.log((row.losses + (args[2] * 1)) + (row.wins * 1))
-        console.log((row.losses + (args[2] * 1)) / ((row.losses + (args[2] * 1)) + (row.wins * 1)))
+        console.log(row.losses + (args[2] * 1));
+        console.log((row.losses + (args[2] * 1)) + (row.wins * 1));
+        console.log((row.losses + (args[2] * 1)) / ((row.losses + (args[2] * 1)) + (row.wins * 1)));
         sql.run(`UPDATE score SET losses = ${row.losses + (args[2] * 1)},
         matches = ${(row.losses + (args[2] * 1)) + (row.wins * 1)},
         percent = ${Math.floor((row.losses + (args[2] * 1)) / ((row.losses + (args[2] * 1)) + (row.wins * 1)) * 1000) / 10}
-        WHERE userId = ${member.id}`)
-      };
-    })
-  };
+        WHERE userId = ${member.id}`);
+      }
+    });
+  }
 
   if (command === "swl") {
     //This is essentially a way to reset scores in case the above starts screwing up.
     //This will be removed in the official release.
     let member = message.mentions.members.first();
-    let wl = args[1]
-    let points = args[2]
+    let wl = args[1];
+    let points = args[2];
     sql.get(`SELECT * FROM score WHERE userId ="${member.id}"`).then(row => {
       if (wl === "w") {
         sql.run(`UPDATE score SET wins = ${points} WHERE userId = ${member.id}`);
@@ -261,7 +262,7 @@ client.on("message", function(message) {
         sql.run(`UPDATE score SET losses = ${points} WHERE userId = ${member.id}`);
       }
     });
-  };
+  }
 
   //testing testing, 1-2-3.
   if (command === "import"){
